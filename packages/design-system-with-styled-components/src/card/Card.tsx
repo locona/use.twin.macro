3c678570d4b2977f4d4a1b2c00255d4c9f8d7ea7
Miyamae Yuuya <locona.0810@gmail.com>
2021-02-16 13:03:15 +0900

    Update Card.tsx

--- packages/design-system-with-styled-components/src/card/Card.tsx
+++ packages/design-system-with-styled-components/src/card/Card.tsx
@@ -13,7 +13,7 @@ export const Card: React.FC<CardProps> = ({ children, title }) => (
     css={[tw`block px-4 py-2 text-sm text-gray-700 bg-white hover:bg-gray-100`]}
   >
     <div>
-      <a href="#">プロフィール</a>
+      <a href="#">Profile</a>
     </div>
   </section>
   // NOTE: Success Case
