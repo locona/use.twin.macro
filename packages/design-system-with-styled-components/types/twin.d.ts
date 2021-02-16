d32acd3b077b24fca3b04d31489c4b31d08f3134
miyamae yuya <locona.0810@gmail.com>
2021-02-16 12:51:38 +0900

    fix(design-system-with-styled-components): fix twin.d.ts

--- packages/design-system-with-styled-components/types/twin.d.ts
+++ packages/design-system-with-styled-components/types/twin.d.ts
@@ -1,15 +1,21 @@
 import 'twin.macro'
-import styledImport from '@emotion/styled'
-import { css as cssImport } from '@emotion/react'
-
-// The css prop
-// https://emotion.sh/docs/typescript#css-prop
-import {} from '@emotion/react/types/css-prop'
+import styledImport, { CSSProp, css as cssImport } from 'styled-components'
 
 declare module 'twin.macro' {
   // The styled and css imports
   const styled: typeof styledImport
   const css: typeof cssImport
+}
+
+declare module 'react' {
+  // The css prop
+  interface HTMLAttributes<T> extends DOMAttributes<T> {
+    css?: CSSProp
+  }
+  // The inline svg css prop
+  interface SVGProps<T> extends SVGProps<SVGSVGElement> {
+    css?: CSSProp
+  }
 }
 
 // The 'as' prop on styled components
